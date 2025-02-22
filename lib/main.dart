99964ba24ad67c99a716f7f2c41257728e24dd8e
nayeem15-5049 <nayeem15-5049@diu.edu.bd>
2025-02-22 04:30:36 +0000

    ached_network_image

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart'; // Import the CachedNetworkImage package
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends
- StatelessWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ImageScreen(),
-    );
+    return MaterialApp(home: ImageScreen());
   }
 }
 
 class ImageScreen extends StatelessWidget {
   const ImageScreen({super.key});
 
-  @
-override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Image Example'),
-      ),
+      appBar: AppBar(title: const Text('Cached Image Example')),
       body: Center(
         child: FittedBox(
           fit: BoxFit.contain,
-          child: Image.network(
-            'https://fastly.picsum.photos/id/8/5000/3333.jpg?hmac=OeG5ufhPYQBd6Rx1TAldAuF92lhCzAhKQKttGfawWuA',
+          child: CachedNetworkImage(
+            imageUrl:
+                'https://fastly.picsum.photos/id/8/5000/3333.jpg?hmac=OeG5ufhPYQBd6Rx1TAldAuF92lhCzAhKQKttGfawWuA',
+            placeholder:
+                (context, url) =>
+                    const CircularProgressIndicator(), // Placeholder widget while loading
+            errorWidget:
+                (context, url, error) => const Icon(
+                  Icons.error,
+                ), // Error widget if the image fails to load
           ),
         ),
       ),
